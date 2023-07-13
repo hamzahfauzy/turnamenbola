edf7cb8827cc0be55420192b1a63f69b8a197ebf
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-07-13 13:04:20 +0700

    add team person lang

--- config/lang.php
+++ config/lang.php
@@ -4,8 +4,10 @@ return [
     'offices' => 'kantor',
     'Team' => 'Tim',
     'team' => 'Tim',
+    'team_persons' => 'Pemain',
     'teams' => 'Tim',
     'tournaments' => 'Turnamen',
+    'tournament_group' => 'Grup Turnamen',
     'tournament_group_team' => 'Tim Grup',
     'tournament_matches' => 'Pertandingan',
     'team_tournaments' => 'Tim Peserta'
